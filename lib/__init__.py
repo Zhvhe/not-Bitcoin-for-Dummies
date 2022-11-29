809eb2f0a95a43d1ca530e582c93a1d6a362a745
zhvhe <zhvhem@gmail.com>
2022-11-28 19:30:10 -0600

    initial upload

---
+++ lib/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os, sys
+
+if(os.sep == '/'):
+    sys.path.insert(0, "../lib")
+else:
+    sys.path.insert(0, "..\\lib")
+
+from .base58 import *
+from .bech32 import *
+from .codes import *
+from .encoder import *
+from .format import *
+from .hash import *
+from .helper import *
+from .ripemd import *
+from .rpc import *
+from .sign import *
